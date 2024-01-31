98eabdb8464002f323bf3e3cc2d2b6abde2951eb
Jeongjihun <wlgns2234@naver.com>
2024-01-31 13:11:55 +0900

    init: setting

---
+++ src/main/java/com/example/sub_module_test/SubModuleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sub_module_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubModuleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubModuleTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sub_module_test/SubModuleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sub_module_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubModuleTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
